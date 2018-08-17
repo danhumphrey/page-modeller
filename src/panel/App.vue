fb16e6bf84b7917cdf58951523162a8d48606be9
Dan Humphrey <danh2000@gmail.com>
2018-08-17 13:23:03 +1000

    refactoring to use Vue components

--- src/panel/App.vue
+++ src/panel/App.vue
@@ -1,75 +1,26 @@
 <template>
     <v-app>
-        <v-toolbar dense>
-            <v-tooltip bottom open-delay="600">
-                <v-btn slot="activator" ref="btn-scan" id="btn-scan" icon v-on:click="inspect" v-bind:class="{active: appData.isInspecting}">
-                    <v-icon>find_in_page</v-icon>
-                </v-btn>
-                <span v-if="!appData.isInspecting">Scan Page</span>
-                <span v-if="appData.isInspecting">Stop Scanning</span>
-
-            </v-tooltip>
-
-            <v-tooltip bottom :disabled="!appData.hasModel" open-delay="600">
-                <v-btn slot="activator" icon disabled >
-                    <v-icon>delete_sweep</v-icon>
-                </v-btn>
-                <span>Delete All Elements</span>
-            </v-tooltip>
-
-            <v-spacer></v-spacer>
-            <!--
-            <v-tooltip bottom open-delay="600">
-                <v-btn slot="activator" icon>
-                    <v-icon>playlist_add</v-icon>
-                </v-btn>
-                <span>Add Element</span>
-            </v-tooltip>
-
-            <v-tooltip bottom open-delay="600">
-                <v-btn slot="activator" icon>
-                    <v-icon>delete</v-icon>
-                </v-btn>
-                <span>Delete Item</span>
-            </v-tooltip>
-
-            <v-tooltip bottom open-delay="600">
-                <v-btn slot="activator" icon>
-                    <v-icon>remove_red_eye</v-icon>
-                </v-btn>
-                <span>View Matches</span>
-            </v-tooltip>
-            -->
-            <v-tooltip bottom :disabled="!appData.hasModel" open-delay="600">
-                <v-btn slot="activator" icon disabled>
-                    <v-icon>code</v-icon>
-                </v-btn>
-                <span>Generate Code</span>
-            </v-tooltip>
-
-        </v-toolbar>
+        <Toolbar @inspect="inspect" :is-inspecting="isInspecting" :has-model="hasModel"/>
     </v-app>
 </template>
 
 <script>
+import Toolbar from './Toolbar';
+
 export default {
   name: 'app',
-  props: {
-    appData: {
-      default: function() {
-        return {
-          isInspecting: false,
-          hasModel: false,
-        };
-      },
-      type: Object,
-    },
+  components: { Toolbar },
+  data: function() {
+    return {
+      isInspecting: false,
+      hasModel: false,
+    };
   },
   methods: {
     inspect: function(e) {
-      this.appData.isInspecting = !this.appData.isInspecting;
+      this.$data.isInspecting = !this.$data.isInspecting;
 
-      if (this.appData.isInspecting) {
+      if (this.isInspecting) {
         chrome.runtime.sendMessage({ type: 'appStartInspecting', data: {} });
       } else {
         chrome.runtime.sendMessage({ type: 'appStopInspecting', data: {} });
@@ -82,7 +33,7 @@ export default {
         if (msg.type === 'elementInspected') {
           console.log('elementInspected message received');
           console.log(msg.data);
-          this.appData.isInspecting = false;
+          this.$data.isInspecting = false;
         }
       });
     });
@@ -90,16 +41,4 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-@import '../styles/settings.scss';
-@import '../styles/material';
-
-button {
-  &:hover {
-    color: $highlight;
-  }
-  &.active {
-    background: $highlight;
-    color: $active-highlight;
-  }
-}
 </style>
---
+++ src/panel/Toolbar.vue
@@ -0,0 +1,77 @@
+<template>
+    <v-toolbar dense>
+        <v-tooltip bottom open-delay="600">
+            <v-btn slot="activator" ref="btn-scan" id="btn-scan" icon v-on:click="inspect" v-bind:class="{active: isInspecting}">
+                <v-icon>find_in_page</v-icon>
+            </v-btn>
+            <span v-if="!isInspecting">Scan Page</span>
+            <span v-if="isInspecting">Stop Scanning</span>
+
+        </v-tooltip>
+
+        <v-tooltip bottom :disabled="!hasModel" open-delay="600">
+            <v-btn slot="activator" icon disabled >
+                <v-icon>delete_sweep</v-icon>
+            </v-btn>
+            <span>Delete All Elements</span>
+        </v-tooltip>
+
+        <v-spacer></v-spacer>
+        <!--
+        <v-tooltip bottom open-delay="600">
+            <v-btn slot="activator" icon>
+                <v-icon>playlist_add</v-icon>
+            </v-btn>
+            <span>Add Element</span>
+        </v-tooltip>
+
+        <v-tooltip bottom open-delay="600">
+            <v-btn slot="activator" icon>
+                <v-icon>delete</v-icon>
+            </v-btn>
+            <span>Delete Item</span>
+        </v-tooltip>
+
+        <v-tooltip bottom open-delay="600">
+            <v-btn slot="activator" icon>
+                <v-icon>remove_red_eye</v-icon>
+            </v-btn>
+            <span>View Matches</span>
+        </v-tooltip>
+        -->
+        <v-tooltip bottom :disabled="!hasModel" open-delay="600">
+            <v-btn slot="activator" icon disabled>
+                <v-icon>code</v-icon>
+            </v-btn>
+            <span>Generate Code</span>
+        </v-tooltip>
+
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  props: ['isInspecting', 'hasModel'],
+  methods: {
+    inspect: function() {
+      this.$emit('inspect');
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '../styles/settings';
+@import '../styles/material';
+
+button {
+  &:hover {
+    color: $highlight;
+  }
+  &.active {
+    background: $highlight;
+    color: $active-highlight;
+  }
+}
+</style>
