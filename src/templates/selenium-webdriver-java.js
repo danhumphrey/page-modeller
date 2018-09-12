8d1b26612fb3a83d99647696e7dae59518843bbb
Dan Humphrey <danh2000@gmail.com>
2018-09-12 15:11:03 +1000

    added a .prettierignore file to exclude templates

--- src/templates/selenium-webdriver-java.js
+++ src/templates/selenium-webdriver-java.js
@@ -10,18 +10,17 @@ ${model.entities
  public WebElement get${entity.name}Element() {
  	${entity.locators
     .map(locator => `${locator.selected ? `return driver.findElement(By.${locator.name === 'css' ? 'cssSelector' : locator.name}("${locator.locator}"));` : ``}`)
-    .join('')}	
+    .join('')
+  }	
  }
-
 ${
-        entity.tagName === 'A' ||
-        entity.tagName === 'BUTTON' ||
-        entity.tagName === 'IMG' ||
-        (entity.tagName === 'INPUT' && ['button', 'reset', 'image', 'submit'].includes(entity.type))
-          ? ` public void click${entity.name}() {\n	get${entity.name}Element().click();\n }`
-          : ''
-      }
- 
+  entity.tagName === 'A' ||
+  entity.tagName === 'BUTTON' ||
+  entity.tagName === 'IMG' ||
+  (entity.tagName === 'INPUT' && ['button', 'reset', 'image', 'submit'].includes(entity.type))
+    ? ` public void click${entity.name}() {\n	get${entity.name}Element().click();\n }`
+    : ''
+}
 `
   )
   .join('')}
