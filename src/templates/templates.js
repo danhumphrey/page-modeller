bdbf5d68d7176d1edfbc789414dfc42ccb8b0a97
Dan Humphrey <danh2000@gmail.com>
2018-09-18 13:06:08 +1000

    Simplified template code into individual render methods

--- src/templates/templates.js
+++ src/templates/templates.js
@@ -1,34 +1,55 @@
-const seleniumwebdriverjava = (model) => {
-  return `
-${model.entities
-  .map(
-    entity =>
-      `
+const isClickable = (entity) => {
+  return  ['A', 'BUTTON', 'IMG'].includes(entity.tagName) || (entity.tagName === 'INPUT' && ['button', 'reset', 'image', 'submit'].includes(entity.type));
+};
+const isInteractive = (entity) => {
+  return ['INPUT', 'A', 'BUTTON', 'IMG', 'SELECT', 'TEXTAREA'].includes(entity.tagName);
+};
+const renderEntityComment = (entity) => {
+  return`
 /*
  * ${entity.name}
  * ***************************************************************
  */
+`;
+};
+const renderFindByLocatorStatement = (locator) => {
+  if(!locator.selected) {
+    return '';
+  }
+  const locatorName = locator.name === 'css' ? 'cssSelector' : locator.name;
+  return `driver.findElement(By.${locatorName}("${locator.locator}"));`;
+};
+const renderGetElementMethod = (entity) => {
+  return `
  public WebElement get${entity.name}Element() {
- 	${entity.locators
-        .map(locator => `${locator.selected ? `return driver.findElement(By.${locator.name === 'css' ? 'cssSelector' : locator.name}("${locator.locator}"));` : ``}`)
-        .join('')
-        }	
+     ${entity.locators.map(locator => renderFindByLocatorStatement(locator)).join('')}
  }
-${
-        entity.tagName === 'A' ||
-        entity.tagName === 'BUTTON' ||
-        entity.tagName === 'IMG' ||
-        (entity.tagName === 'INPUT' && ['button', 'reset', 'image', 'submit'].includes(entity.type))
-          ? ` public void click${entity.name}() {\n	get${entity.name}Element().click();\n }`
-          : ''
-        }
-${
-        ['INPUT', 'A', 'BUTTON', 'IMG', 'SELECT', 'TEXTAREA'].includes(entity.tagName) ? '' : ` public String get${entity.name}() {\n	get${entity.name}Element().getText();\n }`
-        }
-`
-  )
-  .join('')}
-`};
+`;
+};
+const renderClickMethod = (entity) => {
+  if(isClickable(entity)) {
+    return` 
+ public void click${entity.name}() {
+     get${entity.name}Element().click();
+ }`
+  } else {
+    return '';
+  }
+};
+
+const renderGetTextMethod = (entity) => {
+  if(isInteractive(entity)) {
+    return '';
+  }
+  return` 
+ public String get${entity.name}() {
+     get${entity.name}Element().getText();
+ }`
+};
+
+const seleniumwebdriverjava = (model) => {
+  return model.entities.map( entity => `${renderEntityComment(entity)}${renderGetElementMethod(entity)}${renderClickMethod(entity)}${renderGetTextMethod(entity)}`).join('');
+};
 
 export default {
   seleniumwebdriverjava
