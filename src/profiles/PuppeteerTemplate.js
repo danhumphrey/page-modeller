c6477309c9b3bb5e069a7b38bdff88ceae0724b5
Dan Humphrey <danh2000@gmail.com>
2018-09-24 20:27:13 +1000

    Update Puppeteer template

--- src/profiles/PuppeteerTemplate.js
+++ src/profiles/PuppeteerTemplate.js
@@ -1,4 +1,4 @@
-import {isClickable, isInteractive} from "./templates-helpers";
+import { isClickable, isInteractive } from './templates-helpers';
 
 const renderEntityComment = entity => `
 /*
@@ -9,19 +9,18 @@ const renderEntityComment = entity => `
 const getSelectedLocator = entity => entity.locators.find(l => l.selected);
 const renderGetElement = entity => {
   const locator = getSelectedLocator(entity);
-  const method = locator.name === 'css' ? '$' : '$x'; //css or xpath
+  const method = locator.name === 'css' ? '$' : '$x'; // css or xpath
   return `
  async get${entity.name}Element() {
-   return (await this.page.${method}(\`${locator.locator}\`))${locator.locator === 'xpath' ? '[0]' : ''};
+   return (await this.page.${method}(\`${locator.locator}\`))${locator.name === 'xpath' ? '[0]' : ''};
  }  
-`
+`;
 };
 
 const renderClickMethod = entity => {
   if (!isClickable(entity)) {
     return '';
   }
-  let locator = getSelectedLocator(entity);
   return `
  async click${entity.name}() {
    this.get${entity.name}Element().click();
@@ -43,17 +42,27 @@ const renderGetAndSetCheckboxRadio = entity => `
 
 const renderGetAndSetSelect = entity => `
  async get${entity.name}Text() {
-     return this.get${entity.name}Element().;
- }
- async get${entity.name}Value() {
-     return this.get${entity.name}Element().;
- }
- async set${entity.name}ByValue(value) {
-     return this.get${entity.name}Element().;
+   const element = this.get${entity.name}Element();
+   return await (await this.page.evaluate(el => el.options[el.selectedIndex].text, element));
  }
  
- public void set${entity.name}ByText(String text) {
-     return this.get${entity.name}Element().;
+ async get${entity.name}Value() {
+   const element = this.get${entity.name}Element();
+   return await (await page.evaluate(el => el.options[el.selectedIndex].value, element));
+ }
+ 
+ async set${entity.name}ByValue(value) {
+   const element = this.get${entity.name}Element();  
+   await (await page.evaluate(el => {
+     Array.from(el.options).find(o => o.value === value).selected = 'selected';
+   }, element));
+ }
+ 
+ async set${entity.name}ByText(text) {
+   const element = this.get${entity.name}Element();  
+   await (await page.evaluate(el => {
+     Array.from(el.options).find(o => o.text === text).selected = 'selected';
+   }, element));
  }
 `;
 
@@ -86,7 +95,7 @@ const renderGetTextMethod = entity => {
   return `
  async get${entity.name}() {
    const element = await this.get${entity.name}Element();
-   return await page.evaluate(el => el.textContent, el);
+   return await this.page.evaluate(el => el.textContent, el);
  }
 `;
 };
