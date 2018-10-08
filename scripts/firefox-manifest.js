2df8b8ae493c8a09dc98f28963948b1476394f5c
Dan Humphrey <danh2000@gmail.com>
2018-10-08 20:26:37 +1100

    Update minimum firefox version

--- scripts/firefox-manifest.js
+++ scripts/firefox-manifest.js
@@ -6,6 +6,7 @@ const manifest = require('../dist-firefox/manifest.json');
   manifest.applications = {
     gecko: {
       id: '{1e34b9b3-8f45-415e-9586-c7d5de0d0aff}',
+      strict_min_version: '57.0',
     },
   };
   fs.writeFileSync('../dist-firefox/manifest.json', JSON.stringify(manifest, null, 2));
