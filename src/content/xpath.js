1afc68da68c466e7ff3280e736ded5f6ad2085e0
Dan Humphrey <danh2000@gmail.com>
2018-10-24 11:54:25 +1100

    Update improve xpath locators

--- src/content/xpath.js
+++ src/content/xpath.js
@@ -41,7 +41,6 @@ const uniqueXPath = (element, xpath) => {
   const matchingElements = [...dom.findElementsByXPath(element.ownerDocument, xpath)];
   if (matchingElements.length === 1 && matchingElements[0] === element) {
     return xpath;
-    s;
   }
   for (let i = 0; i < matchingElements.length; i += 1) {
     const newXPath = `(${xpath})[${i + 1}]`;
@@ -62,13 +61,49 @@ const getElementNodeName = element => {
   return name;
 };
 
+const getSameSiblingCount = element => {
+  let childNodes = element.parentNode.childNodes;
+  let total = 0;
+  for (let i = 0; i < childNodes.length; i++) {
+    let child = childNodes[i];
+    if (child.nodeName === element.nodeName) {
+      total++;
+    }
+  }
+  return total - 1;
+};
+
+const getIndexOfElement = element => {
+  let childNodes = element.parentNode.childNodes;
+  let total = 0;
+  let index = -1;
+  for (let i = 0; i < childNodes.length; i++) {
+    let child = childNodes[i];
+    if (child.nodeName === element.nodeName) {
+      if (child === element) {
+        index = total;
+      }
+      total++;
+    }
+  }
+  return index;
+};
+
+const getRelativeXPathFromParent = element => {
+  let index = getIndexOfElement(element);
+  let sameSiblingCount = getSameSiblingCount(element);
+  let currentPath = '/' + getElementNodeName(element);
+  if (index > 0 || sameSiblingCount > 0) {
+    currentPath += '[' + (index + 1) + ']';
+  }
+  return currentPath;
+};
+
 // builder methods
 const idBuilder = element => {
   const id = dom.getId(element);
   if (id) {
-    return {
-      xpath: uniqueXPath(element, `//${getElementNodeName(element)}[@id=${attributeValue(id)}]`),
-    };
+    return uniqueXPath(element, `//${getElementNodeName(element)}[@id=${attributeValue(id)}]`);
   }
   return false;
 };
@@ -76,9 +111,38 @@ const idBuilder = element => {
 const nameBuilder = element => {
   const name = dom.getName(element);
   if (name) {
-    return {
-      xpath: uniqueXPath(element, `//${getElementNodeName(element)}[@name=${attributeValue(name)}]`),
-    };
+    return uniqueXPath(element, `//${getElementNodeName(element)}[@name=${attributeValue(name)}]`);
+  }
+  return false;
+};
+
+const ariaLabelBuilder = element => {
+  const label = element.getAttribute('aria-label');
+  if (label) {
+    return uniqueXPath(element, `//${getElementNodeName(element)}[@aria-label=${attributeValue(label)}]`);
+  }
+  return false;
+};
+
+const linkTextBuilder = element => {
+  if (element.nodeName === 'A') {
+    let text = element.textContent;
+    if (!text.match(/^\s*$/)) {
+      return uniqueXPath(element, `//${getElementNodeName(element)}[contains(text(),'${text.replace(/^\s+/, '').replace(/\s+$/, '')}')]`);
+    }
+  }
+  return false;
+};
+
+const imageBuilder = element => {
+  if (element.nodeName === 'IMG') {
+    if (element.alt !== '') {
+      return uniqueXPath(element, `//${getElementNodeName(element)}[@alt=${attributeValue(element.alt)}]`);
+    } else if (element.title !== '') {
+      return uniqueXPath(element, `//${getElementNodeName(element)}[@title=${attributeValue(element.title)}]`);
+    } else if (element.src !== '') {
+      return uniqueXPath(element, `//${getElementNodeName(element)}[@src=${attributeValue(element.src)}]`);
+    }
   }
   return false;
 };
@@ -87,20 +151,51 @@ const absoluteXPathBuilder = element => ({
   xpath: dom.getXPath(element),
 });
 
-const builders = [idBuilder, nameBuilder];
-builders.push(absoluteXPathBuilder);
+const builders = [idBuilder, nameBuilder, ariaLabelBuilder, linkTextBuilder, imageBuilder];
 
-const getXPath = element => {
-  let ret;
+const getPreferredXPath = element => {
   for (let i = 0, j = builders.length; i < j; i += 1) {
     const builder = builders[i];
-    const output = builder(element);
-    if (output) {
-      ret = output.xpath;
-      break;
+    const ret = builder(element);
+    if (ret) {
+      return ret;
     }
   }
-  return ret;
+  return false;
 };
 
+const relativeXPathBuilder = element => {
+  let path = '';
+  let current = element;
+  while (current != null) {
+    if (current.parentNode != null) {
+      path = getRelativeXPathFromParent(current) + path;
+      if (1 === current.parentNode.nodeType) {
+        const preferredParentXPath = getPreferredXPath(current.parentNode);
+        if (preferredParentXPath) {
+          return uniqueXPath(current.parentNode, `${preferredParentXPath}${path}`);
+        }
+      }
+    } else {
+      return false;
+    }
+    current = current.parentNode;
+  }
+  return false;
+};
+
+const getXPath = element => {
+  // regular builders
+  let ret = getPreferredXPath(element);
+  if (ret) {
+    return ret;
+  }
+  // relative builder
+  ret = relativeXPathBuilder(element);
+  if (ret) {
+    return ret;
+  }
+  // final fallback to absolute xpath
+  return absoluteXPathBuilder(element);
+};
 export default getXPath;
