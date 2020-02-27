195d77a89ca1460581714cb5d6ad5073e32e7077
Dan <danh2000@gmail.com>
2020-02-28 07:29:46 +1100

    Remove theme colours

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,20 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-// import colours from '../styles/colours.scss';
 
 Vue.use(Vuetify);
-/*
-const opts = {
-  theme: {
-    highlight: colours.active,
-    primary: '#BDBDBD',
-    secondary: '#607D8B',
-    accent: colours.active,
-    error: '#f44336',
-    warning: '#ffeb3b',
-    info: '#2196f3',
-    success: '#4caf50',
-  },
-};
-*/
+
 export default new Vuetify();
