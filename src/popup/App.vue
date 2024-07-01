7cf5fc593c0c04fde91df310027a11fd7a610ec6
Dan Humphrey <danh2000@gmail.com>
2024-07-01 10:10:01 +1000

    add devtools keyboard shortcut to popup

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -3,7 +3,9 @@
     <v-card>
       <v-card-text class="body-2 pb-0 pt-2">
         <h1 class="title mb-2">Page Modeller {{ version }}</h1>
-        <p>Please open DevTools to use the Page Modeller extension.</p>
+        <p>To use the Page Modeller extension, please open DevTools:</p>
+        <p v-if="isMac"><kbd>Command</kbd> + <kbd>Option</kbd> + <kbd>I</kbd></p>
+        <p v-else><kbd>Control</kbd> + <kbd>Shift</kbd> + <kbd>I</kbd></p>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -19,6 +21,7 @@ export default {
   data() {
     return {
       version: '2.5.0',
+      isMac: navigator.userAgent.toUpperCase().includes('MAC'),
     };
   },
   methods: {
